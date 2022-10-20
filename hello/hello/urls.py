fe823b7ae13b013f95a94afa0ee7877cbb147f6e
macbookpro <soton371@gmail.com>
2022-10-20 22:59:34 +0600

    postmethod

--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from .views import home,contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home, name='home')
+    path('home/', home, name='home'),
+    path('contact/',contact,name='contact'),
 ]
--- hello/hello/views.py
+++ hello/hello/views.py
@@ -1,4 +1,6 @@
 from multiprocessing import context
+from pydoc import render_doc
+from unicodedata import name
 from django.shortcuts import render, HttpResponse
 
 def home(request):
@@ -7,3 +9,14 @@ def home(request):
         'name':name
     }
     return render(request, 'home.html', context)
+
+
+def contact(request):
+    if request.method=="POST":
+        name = request.POST['name']
+        phone = request.POST['phone']
+        content = request.POST['content']
+        print(name)
+        print(phone)
+        print(content)
+    return render(request,'contact.html')
